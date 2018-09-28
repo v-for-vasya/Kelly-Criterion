1b582930227fa821fa22993a9af2c6a9ae36d6e1
v-for-vasya <feynmandiag@gmail.com>
2018-09-27 20:12:06 -0700

    Update kelly_maximize_f.py

--- kelly_maximize_f.py
+++ kelly_maximize_f.py
@@ -2,30 +2,15 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import fmin
 
-import timeit
-
-start = timeit.default_timer()
-"""
-file=pd.read_csv('kelly_data.csv',header=None)
-dat=pd.DataFrame(file) #to dataframe
-dat=dat.values #convert to np.array
-"""
-
-
-file=pd.read_csv('vix_just_returns.csv')  #header=None
+file=pd.read_csv('your_time_series.csv')  #header=None
 dat=pd.DataFrame(file) #to dataframe
 dat=dat.dropna().pct_change() #get returns percentages
 dat=dat.values #convert to np.array
 dat=dat[1:] #remove header
 
 def kelly(x):
-    return np.prod(1+dat*x)
+    return np.prod(1+dat*x) #Kelly vector product of time series
 
-max_x = fmin(lambda x: -kelly(x), 0, maxiter=25) #maxiter=25, ftol=0.01, xtol=.01
+max_x = fmin(lambda x: -kelly(x), 0, maxiter=25) #maxiter=25, ftol=0.01, xtol=.01 to solve for the maximum value
 
-print max_x[0]
-
-
-
-stop = timeit.default_timer()
-print('Simulation time: ', stop - start)
+print max_x[0] # returns the kelly bet for imported time series
